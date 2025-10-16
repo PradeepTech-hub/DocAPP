c70aeb1c7776b94c96492d7a5edab6502e7a7d12
Pradeep M <165541251+PradeepTech-hub@users.noreply.github.com>
2025-10-16 21:15:33 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import connectDB from './config/mongodb.js'
 import connectcloudinary from './config/cloudinary.js'
 import adminRouter from './routes/adminRoute.js'
 import doctorRouter from './routes/doctorRoute.js'
+import userRouter from './routes/userRoute.js'
 
 //app config
 
@@ -22,6 +23,7 @@ app.use(cors())
 // API End Points
 app.use('/api/admin',adminRouter)
 app.use('/api/doctor', doctorRouter)
+app.use('/api/user',userRouter)
 
 
 
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,   // expose to LAN
-    port: 5173,   // optional
-  },
-});
+  server: {port:5174,host: true}
+  
+})
